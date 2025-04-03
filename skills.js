9d8bf4a5e3c94cab657b9ff86ec2a32e40854669
github-cloudlabsuser-1298 <168014950+github-cloudlabsuser-1298@users.noreply.github.com>
2025-04-03 17:22:54 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,12 @@
+// function to convert Celsius
+// to Fahrenheit
+function celsiusToFahrenheit(celsius) {
+    // formula to convert celsius
+    // to Fahrenheit
+    return (celsius * 9 / 5) + 32;
+}
+
+// Driver code
+const celsius = 32;
+console.log(celsius + " Celsius is " +
+    celsiusToFahrenheit(celsius) + " Fahrenheit.");
